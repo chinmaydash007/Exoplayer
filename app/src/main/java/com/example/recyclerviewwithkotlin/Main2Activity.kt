171bd5da8b47b69df6cfadd13ea11e010da1b95d
chinmaydash007 <chinmaydash11@gmail.com>
2020-03-13 21:39:40 +0530

    Added exoplayer

--- app/src/main/java/com/example/recyclerviewwithkotlin/Main2Activity.kt
+++ app/src/main/java/com/example/recyclerviewwithkotlin/Main2Activity.kt
@@ -1,9 +1,8 @@
 package com.example.recyclerviewwithkotlin
 
-import android.R
 import android.net.Uri
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.source.MediaSource
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
@@ -12,23 +11,19 @@ import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.util.Util
 import kotlinx.android.synthetic.main.activity_main2.*
 
-
-class Main2Activity : AppCompatActivity(R.layout.activity_main2) {
+class Main2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
+        setContentView(R.layout.activity_main2)
         val player = SimpleExoPlayer.Builder(this).build()
         var dataSourceFactory: DataSource.Factory =
             DefaultDataSourceFactory(this, Util.getUserAgent(this, "recyclerviewwithkotlin"))
         var videoSource: MediaSource =
             ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(
-                Uri.parse("https://www.youtube.com/watch?v=GNR9El3XWYo")
+                Uri.parse("https://drive.google.com/file/d/13Y-Bldnb32_kUx4bGiqAJOaaL_qddPy8yA/view?usp=sharing")
             )
         player.prepare(videoSource)
         playerView.setPlayer(player);
-
-
     }
 }
---
+++ app/src/main/java/com/example/recyclerviewwithkotlin/Model/MediaObject.kt
@@ -0,0 +1,3 @@
+package com.example.recyclerviewwithkotlin.Model
+
+data class MediaObject (val title:String,val media_url:String,val thumbnail:String,val descriptoin:String)
